f680361186f3658a860f1ee59fa2e940452e503b
deahtom123 <201504200@ingenieria.usac.edu.gt>
2019-10-14 17:23:02 -0600

    Primer Commit

---
+++ Login_Practica2/src/laboratorio_practica2/Laboratorio_Practica2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio_practica2;
+
+/**
+ *
+ * @author Denilson Argueta
+ */
+public class Laboratorio_Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana_Principal ventana= new Ventana_Principal();
+        ventana.setVisible(true);
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # IPC1_Archivos
+Al iniciar el programa tendra 3 botones
+-Boton ingresar: Comprueba en el archivo creado si existe un usuario con esa informacion
+-Boton Nuevo Usuario: Crea un nuevo registro en el archivo para despues poder ingresar con el.(Los campos usuario y contraseña deben de estar llenos para guardar esta info)
+-Boton mostrar usuarios: Muestra los usuarios en consola
